2e350d5f60db94c30e1052080c0dd03b6b54b0a7
Ozsel75 <ozsel75@hotmail.com>
2023-01-10 19:23:49 +0300

    js started

---
+++ app.js
@@ -0,0 +1,3 @@
+const searchInput = document.querySelector("#poke-input");
+const searchBtn = document.querySelector(".btn-search");
+const pokeContainer = document.querySelector(".poke-container");
